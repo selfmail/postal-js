368a6736cfc3c5b2b25f1eb99a566a5ab44c9d40
henri <henri.me@skiff.com>
2024-09-01 19:43:26 +0200

    fixed some things

--- src/index.ts
+++ src/index.ts
@@ -108,8 +108,9 @@ export default class Postal {
             })
         });
 
-        if (!msg.ok || msg.status !== 404) {
-            throw new Error("Fetch error. We could not reach the postal api. Please check your internet connection and the given url. The given url is: " + this.url);
+        if (!msg.ok) {
+            console.log(msg);
+            throw new Error(`Fetch error. We could not reach the postal api. Please check your internet connection and the given url. The given url is: ${this.url} and this is the status text: ${msg.statusText} (${msg.status})`);
         }
 
         const data = await msg.json() as Omit<SendMessageResponse, "success">;
